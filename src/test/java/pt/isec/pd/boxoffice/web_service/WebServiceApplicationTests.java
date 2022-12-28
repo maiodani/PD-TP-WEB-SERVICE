c9ce769afb9c53e7be438a505c0661babc4a69ee
Daniel Maio <maio.dani.maio@gmail.com>
2022-12-28 15:47:27 +0000

    Start

---
+++ src/test/java/pt/isec/pd/boxoffice/web_service/WebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pt.isec.pd.boxoffice.web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
